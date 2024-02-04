73df60c26083f1f7e9ef35c79975f9231c1b1599
Priyanshujawra <109659772+Priyanshujawra@users.noreply.github.com>
2024-02-04 18:28:39 +0530

    first_commit
    date: 04-02-2025

---
+++ homeassets/js/hi-en.js
@@ -0,0 +1,190 @@
+var arrLang = {
+  en: {
+    SKIPP: "Skip to main content",
+    HOME: "Home",
+    ABOUT: "About Us",
+    EXAMINATION: "Examination",
+    ITIHCF: "Mini ITI Form",
+    REGEXMHF: "Examination Form",
+    EXMFEEHF: "Examination Fee Details",
+    EXMGITIHF: "Examnation Guidlines Mini ITI",
+    EXMGUIDMHF: "Examnation Guidlines Main Exams",
+    PREPORT: "Public Reports",
+    MWSD: "Madarsa Wise Student Details",
+    ECWSD: "Exam-Center Wise Student Report",
+    MIR: "Madarsa Institution Report",
+    MITIR: "Mini ITI Report",
+    MSFR: "Madarsa Staff Report",
+    MSAIR: "Madarsa Student & Infrastructure Report",
+    MSRST: "Madarsa Society Renewal Status",
+    IGAOR: "Important GO's & Office Orders",
+    CDHF: "Office Bearers",
+    LGNMDRS: "Madarsa Login",
+    REGISTRID: "Enter Registration Id",
+    PASSWORD: "Enter Password",
+    CAPTCHA: "Enter CAPTCHA*",
+    LOGIN: "LOG IN",
+    BCKHOME: "Home Page",
+    OTHRLOGN: "Login",
+    ADDLGN: "Administrative Login",
+    DMOREGLG: "DMO/Registrar Login",
+    ADLOGTYPE: "Select Login Type",
+    USERTYPE: "Select User Type",
+    SUPERADLBL: "Super Admin Login",
+    SUPERUSR: "Enter UserName",
+    EXMLOGN: "Examination Login",
+    DISTRSEL: "Select District",
+    CENTRSEL: "Select Examination Center",
+    OPRTRLOGN: "Operator Login",
+    LATESTN: "LATEST NEWS",
+    NEWSF: "News Feed",
+    EXAMRESULT: "Exam Results",
+    STDCORNER: "Student Corner",
+    IMPLNK: "IMPORTANT LINK",
+    POLICY: "Website Policies",
+    TANDC: "Terms and Conditions",
+    CONTUS: "Contact Us",
+    HELP: "Help",
+    COUNTER: "Total Visitors",
+    DISCLAIMER: "Disclaimer",
+    FAQ: "FAQs",
+    UPDATEDON: "Last Updated On : 02-May-2023 | 01:00 PM",
+    COPYRIGHT:
+      "Disclaimer: Every effort has been made to ensure accuracy of data on this web site.However these need to be verified by and supplemented with documents issued finally by the concerned authorities. NIC will not be responsible for any decision or claim that is based on the displayed data. This disclaimer is valid for all pages in this website.",
+  },
+  hi: {
+    SKIPP: "मुख्य विषयवस्तु में जाएं",
+    HOME: "मुख्य पृष्ठ",
+    ABOUT: "हमारे बारे में",
+    EXAMINATION: "परीक्षा",
+    ITIHCF: "मिनी-आईटीआई फॉर्म",
+    REGEXMHF: "परीक्षा फॉर्म",
+    EXMFEEHF: "परीक्षा शुल्क विवरण",
+    EXMGITIHF: "परीक्षा दिशानिर्देश मिनी आईटीआई",
+    EXMGUIDMHF: "परीक्षा दिशानिर्देश मुख्य परीक्षा",
+    PREPORT: "सार्वजनिक रिपोर्ट",
+    MWSD: "मदरसा वार छात्र विवरण",
+    ECWSD: "परीक्षा केंद्र वार छात्र रिपोर्ट",
+    MIR: "मदरसा इंस्टीट्यूशन रिपोर्ट",
+    MITIR: "मिनी आईटीआई रिपोर्ट",
+    MSFR: "मदरसा स्टाफ रिपोर्ट",
+    MSAIR: "मदरसा स्टूडेंट एंड इंफ्रास्ट्रक्चर रिपोर्ट",
+    MSRST: "मदरसा सोसायटी नवीनीकरण स्थिति",
+    IGAOR: "महत्वपूर्ण शासनादेश एवं कार्यालय आदेश",
+    CDHF: "संपर्क सूची",
+    LGNMDRS: "मदरसा लॉगिन",
+    REGISTRID: "पंजीकरण आईडी दर्ज करें",
+    PASSWORD: "पासवर्ड दर्ज करें",
+    CAPTCHA: "कैप्चा कोड दर्ज करें*",
+    LOGIN: "लॉगिन",
+    BCKHOME: "मुख्य पृष्ठ पर जाएं",
+    OTHRLOGN: "अन्य लॉगिन",
+    ADDLGN: "प्रशासनिक लॉगिन",
+    DMOREGLG: "डीएमओ/रजिस्ट्रार लॉगिन",
+    ADLOGTYPE: "लॉगिन टाइप चुनें",
+    USERTYPE: "यूजर टाइप चुनें",
+    SUPERADLBL: "सुपर एडमिन लॉगिन",
+    SUPERUSR: "यूजर नाम दर्ज करें",
+    EXMLOGN: "परीक्षा लॉगिन",
+    DISTRSEL: "डिस्ट्रिक्ट चुनें",
+    CENTRSEL: "परीक्षा केंद्र चुनें",
+    OPRTRLOGN: "ऑपरेटर लॉगिन",
+    LATESTN: "नवीन सूचना",
+    NEWSF: "महत्वपूर्ण सूचना",
+    EXAMRESULT: "परीक्षा परिणाम",
+    STDCORNER: "छात्र हेतु",
+    IMPLNK: "महत्वपूर्ण लिंक",
+    POLICY: "वेबसाइट नीतियां",
+    TANDC: "नियम और शर्तें",
+    CONTUS: "संपर्क करें",
+    HELP: "सहायता",
+    COUNTER: "कुल आगंतुक",
+    DISCLAIMER: "डिस्क्लेमर",
+    FAQ: "सामान्य प्रश्न",
+    UPDATEDON: "अंतिम नवीनीकृत तिथि : 28-जुलाई-2021 | 02:45 PM",
+    COPYRIGHT:
+      "डिस्क्लेमर: इस साइट पर डेटा की सटीकता सुनिश्चित करने के लिए हर संभव प्रयास किया गया है। हालांकि इनका सत्यापन,संबंधित अधिकारियों द्वारा अंतत: जारी किए गए दस्तावेजों के आधार पर किया जाना चाहिए। किसी भी प्रदर्शित सामग्री/डाटा के आधार पर उत्पन्न हुए किसी भी निर्णय अथवा दावे के प्रति एनआईसी उत्तरदायी नहीं होगा। यह डिस्क्लेमर इस वेबसाइट के सभी पृष्ठों के लिए मान्य है।",
+  },
+};
+
+$(document).ready(function () {
+  // The default language is English
+
+  mainlogic();
+});
+
+function langdropdown() {
+  var x = document.getElementById("LangSelect").value;
+  setLanguage(x);
+}
+var lang;
+function getLanguage() {
+  if (localStorage.getItem("lang") == null) {
+    setLanguage("en");
+    lang = "en";
+  } else {
+    setLanguage(localStorage.getItem("lang"));
+  }
+}
+
+function mainlogic() {
+  getLanguage();
+  $(".lang").each(function (index, element) {
+    $(this).text(arrLang[lang][$(this).attr("key")]);
+  });
+  $(".inputs").each(function (index, element) {
+    $(this).attr("placeholder", arrLang[lang][$(this).attr("key")]);
+  });
+
+  $(".btnlang").each(function (index, element) {
+    $(this).prop("value", arrLang[lang][$(this).attr("key")]);
+  });
+}
+
+function setLanguage(langx) {
+  localStorage.setItem("lang", langx);
+  lang = langx;
+
+  $("#langhidden").val(langx);
+
+  $(".lang").each(function (index, element) {
+    $(this).text(arrLang[lang][$(this).attr("key")]);
+  });
+  $(".inputs").each(function (index, element) {
+    $(this).attr("placeholder", arrLang[lang][$(this).attr("key")]);
+  });
+
+  $(".btnlang").each(function (index, element) {
+    $(this).prop("value", arrLang[lang][$(this).attr("key")]);
+  });
+
+  if (lang == "en") {
+    $("#li_eng").css("display", "none");
+    $("#li_hin").css("display", "block");
+    $(".resize").css("font-size", "16px");
+    $(".lang-en").css("display", "block");
+    $(".lang-hi").css("display", "none");
+    $("#LangSelect").val("en");
+    // $('.dynamic_link').attr("href", "About.aspx");   /* To bind url dinamically */
+  } else {
+    $("#li_hin").css("display", "none");
+    $("#li_eng").css("display", "block");
+    $(".resize").css("font-size", "16px");
+    $(".lang-en").css("display", "none");
+    $(".lang-hi").css("display", "block");
+    $("#LangSelect").val("hi");
+    // $('.dynamic_link').attr("href", "About_hi.aspx");
+  }
+
+  $(".main-menu li").each(function (index, element) {
+    if ($(this).children("div").length > 0) {
+      $(this).find("> a").append('<span class="indicator">+</span>');
+
+      $(this)
+        .children("a")
+        .click(function (e) {
+          e.preventDefault();
+        });
+    }
+  });
+}
